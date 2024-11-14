5224167b806b20d1c504a2be450ad6803da101c8
Travis Sova <124631581+travis-sova@users.noreply.github.com>
2024-11-14 03:17:19 +0200

    orgunn fuck uppis aga nüüd peaks korras olema (pt2 sest mingi geenius detachis)

--- frontend/Cinema/config/tailwind.config.js
+++ frontend/Cinema/config/tailwind.config.js
@@ -3,15 +3,9 @@
 import daisyui from 'daisyui'
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['.../index.html', '.../src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    daisyui
-  ],
+  plugins: [daisyui],
 }
-
