d8675a9cb2e43a138aa9ee7a4b1899845ab07446
Travis Sova <travis.sova@tptlive.ee>
2024-11-25 12:54:04 +0200

    ee idk

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import MyNavbar from '@/components/dashboard/MyNavbar.vue'
 import MyCarousel from '@/components/MyCarousel.vue'
-import MyShows from './components/MyShows.vue'
+import MyShows from '@/components/MyShows.vue'
+import MyMovie from '@/components/MyMovie.vue'
 </script>
 
 <template>
   <MyNavbar />
   <MyCarousel />
   <MyShows />
+  <MyMovie />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/MyMovie.vue
@@ -0,0 +1,26 @@
+<script lang="ts"></script>
+
+<template>
+  <div class="card card-side bg-secondary max-w-xl shadow-xl m-10 pl-5 py-3 text-primary">
+    <figure>
+      <img
+        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+        alt="Movie"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">Spiderman</h2>
+      <p>Action</p>
+      <div class="card-actions jusify-between flex-row items-stretch">
+        <div>
+          <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
+            <circle r="25" cx="50" cy="50" fill="red" />
+          </svg>
+        </div>
+        <button class="btn btn-primary">Watch</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
