91c49049040b83fcdcae07b7f4fd1cbe84f32226
Travis Sova <travis.sova@tptlive.ee>
2024-11-27 23:40:11 +0200

    routid korras ja lintitud

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,14 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import Home from '@/views/Home.vue'
-import News from '@/views/News.vue'
-import Movies from '@/views/Movies.vue'
-import Cinemas from '@/views/Cinemas.vue'
-import Events from '@/views/Events.vue'
-import Tickets from '@/views/Tickets.vue'
-import Profile from '@/views/Profile.vue'
-import Settings from '@/views/Settings.vue'
+import MyHome from '@/views/MyHome.vue'
+import MyNews from '@/views/MyNews.vue'
+import MyMovies from '@/views/MyMovies.vue'
+import MyCinemas from '@/views/MyCinemas.vue'
+import MyEvents from '@/views/MyEvents.vue'
+import MyTickets from '@/views/MyTickets.vue'
+import MyProfile from '@/views/MyProfile.vue'
+import MySettings from '@/views/MySettings.vue'
 
 import App from './App.vue'
 //import router from './router'
@@ -19,14 +19,14 @@ const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/News', name: 'News', component: News },
-    { path: '/Movies', name: 'Movies', component: Movies },
-    { path: '/Cinemas', name: 'Cinemas', component: Cinemas },
-    { path: '/Events', name: 'Events', component: Events },
-    { path: '/Tickets', name: 'Tickets', component: Tickets },
-    { path: '/Profile', name: 'Profile', component: Profile },
-    { path: '/Settings', name: 'Settings', component: Settings },
+    { path: '/', name: 'Home', component: MyHome },
+    { path: '/News', name: 'News', component: MyNews },
+    { path: '/Movies', name: 'Movies', component: MyMovies },
+    { path: '/Cinemas', name: 'Cinemas', component: MyCinemas },
+    { path: '/Events', name: 'Events', component: MyEvents },
+    { path: '/Tickets', name: 'Tickets', component: MyTickets },
+    { path: '/Profile', name: 'Profile', component: MyProfile },
+    { path: '/Settings', name: 'Settings', component: MySettings },
   ],
 })
 
