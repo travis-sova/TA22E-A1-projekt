9866da07b536c903933531684dbc22d0f6c88164
Travis Sova <travis.sova@tptlive.ee>
2024-12-02 14:00:40 +0200

    ee taun error maybe nussib mujal ära see fix

--- frontend/config/postcss.config.js
+++ frontend/config/postcss.config.js
@@ -5,7 +5,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default {
   plugins: {
-    tailwindcss: { config: path.resolve(__dirname, 'tailwind.config.js') },
+    tailwindcss: { config: path.resolve(__dirname, 'tailwind.config.mjs') },
     autoprefixer: {},
   },
 }
