21c9ba3e241b9a6e892ab77d5fab788c61fe6873
Travis Sova <travis.sova@tptlive.ee>
2024-12-03 02:09:23 +0200

    errori stiil

--- frontend/src/views/My404Error.vue
+++ frontend/src/views/My404Error.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts"></script>
 <template>
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <router-link to="/">Go to Home</router-link>
+  <main class="grid min-h-full place-items-center bg-base-100 px-6 py-24 sm:py-32 lg:px-8">
+    <div class="text-center">
+      <p class="text-base font-semibold text-indigo-600">404</p>
+      <h1
+        class="mt-4 text-balance text-5xl font-semibold tracking-tight text-primary-content sm:text-7xl"
+      >
+        Page not found
+      </h1>
+      <p class="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+        Sorry, we couldn’t find the page you’re looking for.
+      </p>
+      <div class="mt-10 flex items-center justify-center gap-x-6">
+        <router-link
+          to="/"
+          class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >Go back Home</router-link
+        >
+      </div>
     </div>
-  </template>
-  
-  <script lang="ts">
-  export default {
-    name: 'My404Error',
-  };
-  </script>
-  
-  <style scoped>
-  /* Add your styles here */
-  </style>
+  </main>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'My404Error',
+}
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
