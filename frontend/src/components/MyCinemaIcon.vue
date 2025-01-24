470d63d2e6bb72fdd9413641d4b17fbff07ee8b5
trondemynde <ronan.soot@gmail.com>
2025-01-24 18:43:46 +0200

    ROTT-9 Cinemas Page design

---
+++ frontend/src/components/MyCinemaIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    :style="{ top: cinema.position.top, left: cinema.position.left }"
+    class="cinema-icon absolute bg-red-500 hover:bg-red-700 rounded-full w-10 h-10 focus:outline-none focus:ring-4 focus:ring-blue-300 transform hover:scale-125 animate-fadeIn"
+    @click="$emit('selectCinema', cinema)"
+  >
+  🎥
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    cinema: { type: Object, required: true },
+  },
+};
+</script>
