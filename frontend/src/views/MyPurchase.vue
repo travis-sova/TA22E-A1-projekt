12d4242a471bebef28595a01d94277a334660a9d
Personfrommoon <kristofer.v06@gmail.com>
2025-01-28 18:50:36 +0200

    Add event and movie purchase views with routing and data integration

--- frontend/src/views/MyPurchase.vue
+++ frontend/src/views/MyPurchase.vue
@@ -1,15 +1,23 @@
 <template>
-    <div>
-        <h1>Purchase</h1>
-    </div>
+  <div>
+    <h1>Purchase Tickets</h1>
+    <p>Movie: {{ movie.name }}</p>
+    <p>Total Price: {{ total }} €</p>
+  </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+import { useRoute } from 'vue-router';
+import movies from '../../data/movies.js';
 
-const props = defineProps({
-    id: Number,
-    total: Number
-});
+const route = useRoute();
+const movieId = Number(route.params.id);
+const total = Number(route.query.total || 0);
+console.log('Total:', total);
+const movie = movies.find((m) => m.id === movieId);
+
+if (!movie) {
+  throw new Error(`Movie with ID ${movieId} not found`);
+}
 
 </script>
