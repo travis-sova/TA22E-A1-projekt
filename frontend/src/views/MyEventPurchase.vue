84abb6649d6d471d7c63faa647c7aad07305c44b
Personfrommoon <kristofer.v06@gmail.com>
2025-01-29 16:48:54 +0200

    Integrate seat selection in purchase views

--- frontend/src/views/MyEventPurchase.vue
+++ frontend/src/views/MyEventPurchase.vue
@@ -3,20 +3,21 @@
     <h1>Purchase Tickets</h1>
     <p>Event: {{ event.title }}</p>
     <p>Total Price: {{ total }} €</p>
+    <p>Seats: {{ selectedSeats.join(', ') }} </p>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { events } from "../../data/events.js";
 
 const route = useRoute();
 const eventId = Number(route.params.id);
-const total = Number(route.query.total || 0);  // Ensure total is a number
-console.log('Total:', total);
+const total = Number(route.query.total || 0);
+const selectedSeats = ref(route.query.seats ? route.query.seats.split(',') : []);
 
 const event = events.find((e) => e.id === eventId);
-
 if (!event) {
   throw new Error(`Event with ID ${eventId} not found`);
 }
--- frontend/src/views/MyPurchase.vue
+++ frontend/src/views/MyPurchase.vue
@@ -3,21 +3,22 @@
     <h1>Purchase Tickets</h1>
     <p>Movie: {{ movie.name }}</p>
     <p>Total Price: {{ total }} €</p>
+    <p>Seats: {{ selectedSeats.join(', ') }}</p>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import movies from '../../data/movies.js';
 
 const route = useRoute();
 const movieId = Number(route.params.id);
-const total = Number(route.query.total || 0);
-console.log('Total:', total);
-const movie = movies.find((m) => m.id === movieId);
+const total = ref(Number(route.query.total || 0));
+const selectedSeats = ref(route.query.seats ? route.query.seats.split(',') : []);
 
+const movie = movies.find((m) => m.id === movieId);
 if (!movie) {
   throw new Error(`Movie with ID ${movieId} not found`);
 }
-
 </script>
